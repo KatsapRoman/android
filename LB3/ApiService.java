face2223caf4f212dfc6184b44e4cfc0aa8dd039
KatsapRoman <71496483+KatsapRoman@users.noreply.github.com>
2023-11-24 22:06:14 +0200

    Delete LB3 directory

--- LB3/ApiService.java
+++
@@ -1,9 +0,0 @@
-// ApiService.java
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ApiService {
-    @GET("weather")
-    Call<WeatherData> getWeatherData();
-    // Додайте інші методи для інших запитів
-}
